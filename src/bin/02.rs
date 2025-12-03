6aa29a88cf34f4c389b31eee055a610cf448c0bc
Michael <michael.d.long88@gmail.com>
2025-12-02 21:16:24 -0500

    Day 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -25,7 +25,7 @@ pub fn check_if_valid(id: u64) -> u64 {
     let stringified_id = id.to_string();
     let id_length = stringified_id.chars().count();
     let half_length = id_length / 2;
-    if id_length % 2 != 0 {
+    if !id_length.is_multiple_of(2) {
         return 0;
     }
     let first_half = stringified_id.chars().collect::<Vec<_>>()[..half_length]
@@ -38,31 +38,54 @@ pub fn check_if_valid(id: u64) -> u64 {
     if first_half == second_half { id } else { 0 }
 }
 
+pub fn check_if_valid_part2(id: u64) -> u64 {
+    let stringified_id = id.to_string();
+    let id_length = stringified_id.chars().count();
+    let half_length = id_length / 2;
+
+    if id_length >= 2 {
+        let first_char = stringified_id.chars().collect::<Vec<_>>()[0];
+        if stringified_id.chars().all(|digit| digit == first_char) {
+            return id;
+        }
+    }
+
+    for chunk_count in 2..=half_length {
+        let chars: Vec<char> = stringified_id.chars().collect();
+        let char_chunks: Vec<String> = chars
+            .chunks(chunk_count)
+            .map(|chunk| chunk.iter().collect::<String>())
+            .collect();
+
+        let first_chunk = &char_chunks[0];
+        if char_chunks.iter().skip(1).all(|chunk| chunk == first_chunk) {
+            return id;
+        }
+    }
+
+    0
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
     let id_ranges = parse(input);
 
     let invalid_sum = id_ranges
         .iter()
-        .map(|id_range| {
-            id_range
-                .clone()
-                .into_iter()
-                .map(|id| {
-                    let transformed_num = check_if_valid(id);
-                    // if transformed_num != 0 {
-                    //     println!("Invalid ID: {}", transformed_num);
-                    // }
-                    transformed_num
-                })
-                .sum::<u64>()
-        })
+        .map(|id_range| id_range.clone().map(check_if_valid).sum::<u64>())
         .sum();
 
     Some(invalid_sum)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let id_ranges = parse(input);
+
+    let invalid_sum = id_ranges
+        .iter()
+        .map(|id_range| id_range.clone().map(check_if_valid_part2).sum::<u64>())
+        .sum();
+
+    Some(invalid_sum)
 }
 
 #[cfg(test)]
@@ -78,6 +101,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4_174_379_265));
     }
 }
